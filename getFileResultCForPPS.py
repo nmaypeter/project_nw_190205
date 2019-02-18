4b31ae73f0c0737bbc9fe60eaa250fa2ba5d1ca6
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-02-18 16:47:09 +0800

    Update getFileResultCForPPS.py
    ver1.1

--- getFileResultCForPPS.py
+++ getFileResultCForPPS.py
@@ -1,5 +1,5 @@
 for m in [2, 3]:
-    model_name = "mngic" * (m == 1) + "mhdic" * (m == 2) + "mric" * (m == 3) + "_pps"
+    model_name = "mngic" * (m == 1) + "mhdic" * (m == 2) + "mric" * (m == 3) + "mhadic" * (m == 4) + "mpmisic" * (m == 5) + "_pps"
     profit = []
     for data_setting in [2]:
         data_set_name = "email_directed" * (data_setting == 1) + "email_undirected" * (data_setting == 2) + \
