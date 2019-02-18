95d39d740b2b42e2d88717a647e1549ad372afe3
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-02-18 21:17:02 +0800

    Update getFileResultC.py

--- getFileResultC.py
+++ getFileResultC.py
@@ -7,13 +7,13 @@ for data_setting in [2]:
         for prod_setting2 in [1, 2, 3]:
             product_name = "r1p3n" + str(prod_setting) + "a" * (prod_setting2 == 2) + "b" * (prod_setting2 == 3)
             for wpiwp in [bool(0), bool(1)]:
-                for m in [2, 3]:
+                for m in [1, 2, 3, 4, 5]:
                     model_name = "mngic" * (m == 1) + "mhdic" * (m == 2) + "mric" * (m == 3) + "mhadic" * (m == 4) + "mpmisic" * (m == 5) + "_pps"
                     for pps in [1, 2, 3]:
 
                         try:
                             result_name = "result/r/" + model_name + str(pps) + "_wpiwp" * wpiwp + "/" + \
-                                          model_name + str(pps) + "_wpiwp" * wpiwp + "_" + data_set_name + "_" + product_name + "/profit.txt"
+                                          model_name + str(pps) + "_wpiwp" * wpiwp + "_" + data_set_name + "_" + product_name + "/1profit.txt"
                             print(result_name)
 
                             with open(result_name) as f:
@@ -29,9 +29,9 @@ for data_setting in [2]:
 
 fw = open("result/comparison_profit.txt", 'w')
 for lnum, line in enumerate(profit):
-    if lnum % 9 == 0 and lnum != 0:
+    if lnum % 15 == 0 and lnum != 0:
         fw.write("\n")
-    if lnum % 18 == 0 and lnum != 0:
+    if lnum % 30 == 0 and lnum != 0:
         fw.write("\n")
     fw.write(str(line) + "\n")
 fw.close()
